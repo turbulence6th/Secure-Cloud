5b0dd7df4c1fb057d43022a8524c6e698a457b27
turbulence6th <oguz.tanrikulu@metu.edu.tr>
2017-03-21 18:11:34 +0300

    application added

---
+++ application/main.js
@@ -0,0 +1,37 @@
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/apps/app.runtime.html
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+chrome.app.runtime.onLaunched.addListener(function() {
+  // Center window on screen.
+  var screenWidth = screen.availWidth;
+  var screenHeight = screen.availHeight;
+  var width = 500;
+  var height = 300;
+
+  chrome.app.window.create('index.html', {
+    id: "Secure Cloud",
+    outerBounds: {
+      width: width,
+      height: height,
+      left: Math.round((screenWidth-width)/2),
+      top: Math.round((screenHeight-height)/2)
+    }
+  });
+
+  console.log("asdasd")
+
+  chrome.runtime.onMessageExternal.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "hello")
+      sendResponse({farewell: "goodbye"});
+  });
+
+  
+
+});
