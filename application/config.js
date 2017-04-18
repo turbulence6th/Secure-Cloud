1dd8dc1844491b4611f576f267af1949bdf4a007
turbulence6th <oguz.tanrikulu@metu.edu.tr>
2017-04-18 18:10:41 +0300

    asd

--- application/config.js
+++ application/config.js
@@ -58,24 +58,3 @@ function import_private_key(exportedPrivateKey) {
 	    //console.error(err);
 	});
 }
-
-function arrayBufferToBase64(arrayBuffer) {
-    var byteArray = new Uint8Array(arrayBuffer);
-    var byteString = '';
-    for(var i=0; i < byteArray.byteLength; i++) {
-        byteString += String.fromCharCode(byteArray[i]);
-    }
-    var b64 = window.btoa(byteString);
-
-    return b64;
-}
-
-function base64ToArrayBuffer(b64) {
-    var byteString = window.atob(b64);
-    var byteArray = new Uint8Array(byteString.length);
-    for(var i=0; i < byteString.length; i++) {
-        byteArray[i] = byteString.charCodeAt(i);
-    }
-
-    return byteArray;
-}
