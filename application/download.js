299562c771f1f712070ac8b7a562bf5f826ed9ab
Ramazan Zor <ramazanzor19@gmail.com>
2017-05-24 21:42:04 +0300

    share fix

--- application/download.js
+++ application/download.js
@@ -1,19 +1,17 @@
 function decryptTheFile(ciphertext, iv, secretiv, filename,privateKey,sessionKey,secretKey) {
     // Click handler. Reads the selected file, then decrypts it to
     // the random key pair's private key. Creates a Blob with the result,
-  
-    var data;
 
     var encrypted = forge.util.decode64(ciphertext);
-
+    var input = forge.util.createBuffer(encrypted);
     if (secretKey) {
         secretKey = decodeURIComponent(escape(atob(secretKey)));
         secretKey = privateKey.decrypt(secretKey, 'RSA-OAEP');
         var decipher = forge.cipher.createDecipher('AES-CBC', secretKey);
         decipher.start({iv: secretiv});
-        decipher.update(sessionKey);
+        decipher.update(forge.util.createBuffer(sessionKey));
         decipher.finish();
-        sessionKey = decipher.output.getBytes();
+        sessionKey = decipher.output.data;
         sessionKey = privateKey.decrypt(sessionKey, 'RSA-OAEP'); 
         var decipher2 = forge.cipher.createDecipher('AES-CBC', sessionKey);
         decipher2.start({iv: iv});
@@ -22,7 +20,7 @@ function decryptTheFile(ciphertext, iv, secretiv, filename,privateKey,sessionKey
         // outputs decrypted hex
         var decrypted64 = decipher2.output.getBytes();
     } else {   
-        var input = forge.util.createBuffer(encrypted);
+
         sessionKey = privateKey.decrypt(sessionKey, 'RSA-OAEP'); 
         var decipher = forge.cipher.createDecipher('AES-CBC', sessionKey);
         decipher.start({iv: iv});
